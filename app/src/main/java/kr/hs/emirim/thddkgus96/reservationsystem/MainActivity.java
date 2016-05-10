bb99ef6add4a45d3fa54c234c72d579bf9b71247
Ahhyeon Song <thddkgus36@gmail.com>
2016-05-10 20:07:04 +0900

    #3 Second EventProgramming

--- app/src/main/java/kr/hs/emirim/thddkgus96/reservationsystem/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/thddkgus96/reservationsystem/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.thddkgus96.reservationsystem;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
         textResult=(TextView)findViewById(R.id.text_result);
         timePick.setVisibility(View.INVISIBLE);
         calView.setVisibility(View.INVISIBLE);
+
+        radioDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.VISIBLE);
+                timePick.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        radioTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calView.setVisibility(View.INVISIBLE);
+                timePick.setVisibility(View.VISIBLE);
+            }
+        });
 
     }
 }
